d18c16dc31b28f7d07bab510fa6432a6f911bf93
Celine Argyropoulos <42405999+ECeline@users.noreply.github.com>
2018-09-04 11:09:09 +0200

    Update 3-___-refactor-test.js

--- test/3-___-refactor-test.js
+++ test/3-___-refactor-test.js
@@ -1,9 +1,9 @@
 const assert = require('tressa');
 const cases = require('./test-cases.js');
 
-const ____refactor = require("../3-___-refactor-test.js");
+const ____refactor = require("../3-___-refactor.js");
 
-assert.title('___-refacotr');
+assert.title('___-refactor');
 
 for (let cond of cases) {
 	assert(____refactor(...cond.input) === cond.expected, cond.message);
